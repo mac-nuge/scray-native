b60f450ffc695937c519b897a1c42078a1a49125
macnuge <betty.elms@gmail.com>
2026-08-02 09:38:24 -0400

    replace fragile addResourceFile with a build-time copy script

--- plugins/withWebAssets.js
+++ plugins/withWebAssets.js
@@ -1,34 +1,22 @@
-const { withDangerousMod, withXcodeProject } = require('@expo/config-plugins');
-const fs = require('fs');
-const path = require('path');
-
-function copyRecursive(src, dest) {
-  fs.mkdirSync(dest, { recursive: true });
-  for (const entry of fs.readdirSync(src, { withFileTypes: true })) {
-    const srcPath = path.join(src, entry.name);
-    const destPath = path.join(dest, entry.name);
-    if (entry.isDirectory()) copyRecursive(srcPath, destPath);
-    else fs.copyFileSync(srcPath, destPath);
-  }
-}
+const { withXcodeProject } = require('@expo/config-plugins');
 
 module.exports = function withWebAssets(config) {
-  config = withDangerousMod(config, [
-    'ios',
-    async (config) => {
-      const src = path.join(config.modRequest.projectRoot, 'assets', 'web');
-      const dest = path.join(config.modRequest.platformProjectRoot, config.modRequest.projectName, 'web');
-      copyRecursive(src, dest);
-      return config;
-    },
-  ]);
-
-  config = withXcodeProject(config, (config) => {
+  return withXcodeProject(config, (config) => {
     const project = config.modResults;
     const target = project.getFirstTarget().uuid;
-    project.addResourceFile('web', { target, lastKnownFileType: 'folder' });
+
+    project.addBuildPhase(
+      [],
+      'PBXShellScriptBuildPhase',
+      'Copy Web Assets',
+      target,
+      {
+        shellPath: '/bin/sh',
+        shellScript:
+          'mkdir -p "${BUILT_PRODUCTS_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}/web" && cp -R "${SRCROOT}/../assets/web/." "${BUILT_PRODUCTS_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}/web/"',
+      }
+    );
+
     return config;
   });
-
-  return config;
 };

08150a957d80f7d623642f6477b716b4bdd05d6f
macnuge <betty.elms@gmail.com>
2026-08-13 14:32:41 -0400

    testing: new icon

--- app.config.js
+++ app.config.js
@@ -4,8 +4,10 @@ module.exports = ({ config }) => {
   return {
     ...config,
     name: IS_DEV_VARIANT ? 'Scray Picker (Dev)' : 'Scray Picker',
+    ...(IS_DEV_VARIANT ? {} : { icon: './assets/images/icon-release.png' }),
     ios: {
       ...config.ios,
+      ...(IS_DEV_VARIANT ? {} : { icon: './assets/images/icon-release.png' }),
       bundleIdentifier: IS_DEV_VARIANT
         ? 'com.mac.scraynative.dev'
         : 'com.mac.scraynative',
